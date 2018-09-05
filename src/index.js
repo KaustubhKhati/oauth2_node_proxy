b63a8ee7434cc996312524f5a4d7226976b21399
Khati, Kaustubh <Kaustubh.Khati@in.tesco.com>
2018-09-05 19:54:16 +0530

    added views and support for additional dashboards.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index');
+	var dashboards = [
+        { name: 'Kubernetes Dashboard', link: 'https://dashboard.example.com', info: 'This will show you kubnertes dashboard' },
+        { name: 'Grafana Dashboard', link: 'https://grafana.example.com', info: 'This will show you grafana dashboards' }
+    ];
+  	res.render('index', {dashboards: dashboards});
 })
 
 app.listen(3000, function(req, res){
